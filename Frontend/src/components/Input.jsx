1c8189589fe5ae047f07bf934d335e28b3157ad7
John <johnlin8968@gmail.com>
2025-04-07 17:37:38 +1000

    update styling

--- Frontend/src/components/Input.jsx
+++ Frontend/src/components/Input.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-function Input({ field, func, value, type = "text" }) {
+function Input({ field, func, value, type = "text", className = "" }) {
   const [show, setShow] = useState(type !== "password");
   const isPassword = type === "password";
 
