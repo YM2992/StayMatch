5c18d15fde03b5414ea8d099f8cc274650ade127
Ason-14 <143095061+Ason-14@users.noreply.github.com>
2025-04-28 17:28:54 +1000

    styling edits

--- Frontend/src/pages/Registration.jsx
+++ Frontend/src/pages/Registration.jsx
@@ -120,7 +120,7 @@ function Registration() {
             <select
               value={securityQuestion}
               onChange={(e) => setSecurityQuestion(e.target.value)}
-              className="w-full h-10 px-4 py-2 border rounded-md shadow-sm bg-white text-gray-700"
+              className="w-full h-10 px-3 py-2 border rounded-md shadow-sm bg-white text-gray-700"
             >
               <option value="">Select a question...</option>
               <option value="What is your favourite sports team?">What is your favourite sports team?</option>
